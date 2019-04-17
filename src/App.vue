b7b29cf5da8a1ccd9dae98ec5d3c234176ab72e0
Peterson Zeferino <petersonzeferino@hotmail.com>
2019-04-17 07:27:37 -0300

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <h2>Conversor de Moedas</h2>
+
+    <div id="linha">
+      <Conversor moedaA="USD" moedaB="BRL"/>
+      <Conversor moedaA="BRL" moedaB="USD"/>
+    </div>
+    <div id="linha">
+      <Conversor moedaA="CAD" moedaB="BRL"/>
+      <Conversor moedaA="BRL" moedaB="CAD"/>
+    </div>
+    <div id="linha">
+      <Conversor moedaA="EUR" moedaB="BRL"/>
+      <Conversor moedaA="BRL" moedaB="EUR"/>
+    </div>
   </div>
+
 </template>
+ 
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Conversor from './components/Conversor.vue' 
 
 export default {
+
   name: 'app',
   components: {
-    HelloWorld
-  }
+     Conversor
+   }
+
 }
-</script>
+
+</script> 
 
 <style>
+
 #app {
+   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   text-align: center;
+   color: #2c3e50;
+   margin-top: 60px;
+
+} 
+
+#linha{
+  display: flex;
+  margin-top: 40px;
+  justify-content: space-around;
+} 
+
+#h2{
+  text-align: center;
+} 
+
+#app{
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+  margin: auto;
+  max-width: 900px;
+} 
+
 </style>
